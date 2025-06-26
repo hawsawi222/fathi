189cdec959e0afffce21bbf430bf8f5ab816b76e
Glitch (glitch-hello-node) <none>
2025-06-26 11:00:22 +0000

    📅💼 Checkpoint ./tokens.js:81688682/698

--- tokens.js
+++ tokens.js
@@ -1,8 +1,8 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1386157570548564028",
-        serverId: "1346788139724963840",
+        channelId: "1308045324480282715",
+        serverId: "1109636583642103903",
         token: process.env.token1,
         selfDeaf: false,
         autoReconnect: {
@@ -11,7 +11,23 @@ export default [
             maxRetries: 5,
         },
         presence: {
-            status: "idle",
+            status: "dnd",
+        },
+        selfMute: false,
+    },
+  
+      {
+        channelId: "1386157650798055545",
+        serverId: "1346788139724963840",
+        token: process.env.token2,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "dnd",
         },
         selfMute: false,
     },
